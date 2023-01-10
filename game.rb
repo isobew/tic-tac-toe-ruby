bfcb407d711c1466d2328ea649d3058962c26247
Isabella Firmino <isabella.mfbr@gmail.com>
2023-01-10 01:49:11 -0400

    Creating Winner function

--- game.rb
+++ game.rb
@@ -5,7 +5,7 @@ class Game
     @hum = "O" 
     @valid = true
     @winner = nil
-    @best_move = nil
+    @finished = false
   end
 
   def start_game
@@ -23,12 +23,14 @@ class Game
     if tie(@board)
       puts "Ninguém venceu"
     end
+
+    winner(@board)
     # puts "Game over"
   end
 
   def show_board(board)
-    puts " #{board[0]} | #{board[1]} | #{board[2]} \n===+===+===\n #{board[3]} | #{board[4]} | #{board[5]} \n===+===+===\n #{board[6]} | #{board[7]} | #{board[8]} \n"
-    puts "Enter [0-8]:"
+    puts "\n #{board[0]} | #{board[1]} | #{board[2]} \n===+===+===\n #{board[3]} | #{board[4]} | #{board[5]} \n===+===+===\n #{board[6]} | #{board[7]} | #{board[8]} \n"
+    puts "\nInsert a number [0-8]:\n"
   end
 
   def get_human_spot(board)
@@ -47,41 +49,42 @@ class Game
       spot = gets.chomp
 
       until available_spaces.include?(spot)
-        puts "Inclua um numero"
+        puts "\nPLEASE, INSERT A VALID NUMBER\n"
         @valid = false
         return
       end
 
-      if available_spaces.include?(spot)
-        spot = spot.to_i
-        @valid == true
-        if @board[spot] != "X" && @board[spot] != "O"
-            @board[spot] = @hum
-            @valid = true
-        end
-      else
-        spot = nil
+      spot = spot.to_i
+      @valid == true
+      if @board[spot] != "X" && @board[spot] != "O"
+        @board[spot] = @hum
+        @valid = true
       end
     end
   end
 
   def eval_board
+    
     spot = nil
+
     until spot
       if @board[4] == "4" && @valid != false
         spot = 4
         @board[spot] = @com
       else
+
         if @valid == false
           return
         else  
           spot = get_best_move(@board, @com)
         end
-          if @board[spot] != "X" && @board[spot] != "O"
+        
+        if @board[spot] != "X" && @board[spot] != "O"
           @board[spot] = @com
         else
           spot = nil
         end
+
       end
     end
   end
@@ -119,14 +122,11 @@ class Game
     else
       n = rand(0..available_spaces.count)
       m = available_spaces[n].to_i
-      puts m
-      puts "maquina acima"
       return m
     end
   end
 
   def game_is_over(b)
-
     [b[0], b[1], b[2]].uniq.length == 1 ||
     [b[3], b[4], b[5]].uniq.length == 1 ||
     [b[6], b[7], b[8]].uniq.length == 1 ||
@@ -135,12 +135,27 @@ class Game
     [b[2], b[5], b[8]].uniq.length == 1 ||
     [b[0], b[4], b[8]].uniq.length == 1 ||
     [b[2], b[4], b[6]].uniq.length == 1
-
-    
   end
 
   def tie(b)
     b.all? { |s| s == "X" || s == "O" }
+  end
+
+  def winner(board)
+    b1 = [board[0], board[1], board[2]]
+    b2 = [board[3], board[4], board[5]]
+    b3 = [board[6], board[7], board[8]]
+    b4 = [board[0], board[3], board[6]]
+    b5 = [board[1], board[4], board[7]]
+    b6 = [board[2], board[5], board[8]]
+    b7 = [board[0], board[4], board[8]]
+    b8 = [board[2], board[4], board[6]]
+
+    if (b1.uniq[0] || b2.uniq[0] || b3.uniq[0] || b4.uniq[0] || b5.uniq[0] || b6.uniq[0] || b7.uniq[0] || b8.uniq[0]) == "X"
+      puts "X wins"
+    elsif (b1.uniq[0] || b2.uniq[0] || b3.uniq[0] || b4.uniq[0] || b5.uniq[0] || b6.uniq[0] || b7.uniq[0] || b8.uniq[0]) == "O"
+      puts "O wins"  
+    end 
   end
 
 end
